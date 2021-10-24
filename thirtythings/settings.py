ed738d7628b8729a12195810f1c8c6126e8a88be
Jon Lawrence <24703763+lampyjon@users.noreply.github.com>
2021-10-24 18:02:14 +0100

    sorting out DB

--- thirtythings/settings.py
+++ thirtythings/settings.py
@@ -74,15 +74,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "thirtythings.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -118,3 +110,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+}
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
